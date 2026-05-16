49cacedcfc51cab5323d1e92bede24d313529e98
lopez9090 <jan.myziak@op.pl>
2026-05-16 14:44:54 +0200

    Added DTOs

---
+++ ApbdCw7/DTOs/PcDTOs.cs
@@ -0,0 +1,55 @@
+﻿namespace ApbdCw7.DTOs;
+
+public class PcResponseDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public double Weight { get; set; }
+    public int Warranty { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int Stock { get; set; }
+}
+
+public class PcRequestDto
+{
+    public string Name { get; set; } = null!;
+    public double Weight { get; set; }
+    public int Warranty { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int Stock { get; set; }
+}
+
+public class PcWithComponentsResponseDto : PcResponseDto
+{
+    public List<PcComponentResponseDto> Components { get; set; } = new();
+}
+
+public class PcComponentResponseDto
+{
+    public int Amount { get; set; }
+    public ComponentDto Component { get; set; } = null!;
+}
+
+public class ComponentDto
+{
+    public string Code { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public ManufacturerDto Manufacturer { get; set; } = null!;
+    public TypeDto Type { get; set; } = null!;
+}
+
+public class ManufacturerDto
+{
+    public int Id { get; set; }
+    public string Abbreviation { get; set; } = null!;
+    public string FullName { get; set; } = null!;
+    public string FoundationDate { get; set; } = null!;
+}
+
+public class TypeDto
+{
+    public int Id { get; set; }
+    public string Abbreviation { get; set; } = null!;
+    public string Name { get; set; } = null!;
+}
