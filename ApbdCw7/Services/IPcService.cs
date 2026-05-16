7609835c7fd92b7884e41c1b88f42c62353936bf
lopez9090 <jan.myziak@op.pl>
2026-05-16 14:59:52 +0200

    Added Services

---
+++ ApbdCw7/Services/IPcService.cs
@@ -0,0 +1,12 @@
+﻿using ApbdCw7.DTOs;
+
+namespace ApbdCw7.Services;
+
+public interface IPcService
+{
+    Task<IEnumerable<PcResponseDto>> GetAllPcsAsync();
+    Task<PcWithComponentsResponseDto?> GetPcWithComponentsAsync(int id);
+    Task<PcResponseDto> CreatePcAsync(PcRequestDto dto);
+    Task<bool> UpdatePcAsync(int id, PcRequestDto dto);
+    Task<bool> DeletePcAsync(int id);
+}
