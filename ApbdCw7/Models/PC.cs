80a8902d54b3d2459650f8ef9b2a9d95b3601e02
lopez9090 <jan.myziak@op.pl>
2026-05-16 14:06:09 +0200

    Added models

---
+++ ApbdCw7/Models/PC.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApbdCw7.Models;
+
+public class PC
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    public double Weight { get; set; }
+
+    [Required]
+    public int Warranty { get; set; }
+
+    [Required]
+    public DateTime CreatedAt { get; set; }
+
+    [Required]
+    public int Stock { get; set; }
+    
+    public ICollection<PCComponent> PCComponents { get; set; } = new List<PCComponent>();
+}
---
+++ ApbdCw7/Models/PCComponent.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApbdCw7.Models;
+
+public class PCComponent
+{
+    public int PCId { get; set; }
+    [ForeignKey(nameof(PCId))]
+    public PC PC { get; set; } = null!;
+
+    [MaxLength(10)]
+    [Column(TypeName = "char(10)")]
+    public string ComponentCode { get; set; } = string.Empty;
+    [ForeignKey(nameof(ComponentCode))]
+    public Component Component { get; set; } = null!;
+
+    [Required]
+    public int Amount { get; set; }
+}
